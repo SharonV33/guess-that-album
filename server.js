caa6ad5ecbf7279958c8509bb9704235693f76d3
sharonV33 <sharonveldman33@gmail.com>
2021-04-06 11:31:15 +0200

    implement code from justus

--- server.js
+++ server.js
@@ -3,17 +3,27 @@ const app = express()
 const http = require('http').createServer(app)
 const path = require('path')
 const compression = require('compression')
+const io = require('socket.io')(http)
+const port = process.env.PORT || 8080
 
-const port = 8080
 
 app.use(express.static(path.resolve('public')))
 app.use(compression())
 
-app.get('/', (request, response) => {
-    response.send('hoi')
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+
+    socket.on('message', (message) => {
+        // console.log('message: ' + message)
+        io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
 })
 
-//the "process.env.PORT" is specific for Heroku deployment
-http.listen(process.env.PORT || port, () => {
-    console.log(`Server running at http://localhost:${port}/`)
+http.listen(port, () => {
+    console.log('listening on port ', port)
 })
