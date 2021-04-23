cdcdff8d7543d8bbe323efce142707ab3eb06045
sharonV33 <sharonveldman33@gmail.com>
2021-04-23 10:57:07 +0200

    add nodemon as devdependency clean up leaderboard

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -1,11 +1,10 @@
-
 const socket = io()
 const user = document.querySelector('#username')
 const guess = document.querySelector('#guess')
 const form = document.querySelector('form')
-// const leaderboard = document.querySelector('#leaderboard')
 
 form.addEventListener('submit', (event) => {
+    //prevent page from reloading
     event.preventDefault()
     if (guess.value && user.value) {
         socket.emit('message', {
@@ -14,49 +13,49 @@ form.addEventListener('submit', (event) => {
             type: 'guess'
         })
 
+        //clear guess input field
         guess.value = ''
     }
 })
 
 socket.on('message', function(message) {
+    //check the type of message
     switch (message.type){
-        case 'updateLeaderboard':
-
-
-            break;
-
         case 'albumGuessed':
+            //make li for each leaderboard entry
             const leaderBoard = JSON.parse(message.leaderBoard)
-            console.log('leaderboard', leaderBoard)
             const leaderboardItems = leaderBoard
                 .map((item) => `<li>${item.name}: ${item.score}</li>`)
                 .join('')
 
+            //add leaderboard content to client
             document
                 .querySelector('.leaderboard ol')
                 .innerHTML = leaderboardItems
-            // Set blur to false and show winner's name
+            //unblur album
             document
                 .querySelector('#blurAlbum')
                 .removeAttribute('checked')
+            //show a message containing the winner's name
             document
                 .querySelector('.winner')
                 .innerHTML = `congrats to ${message.winner} for guessing the album`
             break;
 
         case 'newAlbum':
-            // Set blur to true
-            // load new album art
             const newAlbum = JSON.parse(message.album)
 
+            // Set blur to true
             document
                 .querySelector('#blurAlbum')
                 .setAttribute('checked', 'checked')
 
+            // load new album art
             document
                 .querySelector('#albumArtwork')
                 .setAttribute('src', newAlbum.image[3]['#text'])
 
+            //clear winner message
             document
                 .querySelector('.winner')
                 .innerHTML = ``
